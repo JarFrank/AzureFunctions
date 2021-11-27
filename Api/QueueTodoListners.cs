e9a8774a7db6c7189e3bce2f76bb4db5a0e19a74
Jaroslaw Frankowski <jfrankowski@objectivity.co.uk>
2021-11-27 23:49:23 +0100

    Add project files.

---
+++ Api/QueueTodoListners.cs
@@ -0,0 +1,22 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public static class QueueTodoListners
+    {
+        [FunctionName("QueueTodoListners")]
+        public static async Task Run([QueueTrigger("todos", Connection = "AzureWebJobsStorage")] Todo todo,
+            [Blob("todos", Connection = "AzureWebJobsStorage")] CloudBlobContainer container,
+            ILogger log)
+        {
+            await container.CreateIfNotExistsAsync();
+            var blob = container.GetBlockBlobReference($"{todo.Id}.txt");
+            await blob.UploadTextAsync($"Created a new task: {todo.TaskDescription}");
+            log.LogInformation($"C# Queue trigger function processed: {todo.TaskDescription}");
+        }
+    }
+}
